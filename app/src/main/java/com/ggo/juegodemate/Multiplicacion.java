3541585fcef61e659d5afe07829c2cc4e2da6785
Alan Rocha <alan.rocha@udem.edu>
2019-11-24 01:37:35 -0600

    Added multiplicar menu

--- app/src/main/java/com/ggo/juegodemate/Multiplicacion.java
+++ app/src/main/java/com/ggo/juegodemate/Multiplicacion.java
@@ -2,13 +2,35 @@ package com.ggo.juegodemate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class Multiplicacion extends AppCompatActivity {
+
+    private Button empezar;
+    private ImageView dado;
+    private EditText numero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplicacion);
+        empezar = (Button) findViewById(R.id.empezar_multi);
+        dado = (ImageView) findViewById(R.id.dado);
+        numero = (EditText) findViewById(R.id.numero);
     }
+
+
+    public void random(View v){
+        numero.setText( String.valueOf(new Random().nextInt(10 - 1 + 1) + 1) );
+    }
+
+
+
 }
