9af57a1c83542434c801c7f6e3a74a57ceafcb7e
Alan Rocha <alan.rocha@udem.edu>
2019-11-24 02:11:17 -0600

    Menu Multiplicacion

---
+++ app/src/main/java/com/ggo/juegodemate/JuegoMultiplicacion.java
@@ -0,0 +1,14 @@
+package com.ggo.juegodemate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JuegoMultiplicacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego_multiplicacion);
+    }
+}
--- app/src/main/java/com/ggo/juegodemate/Multiplicacion.java
+++ app/src/main/java/com/ggo/juegodemate/Multiplicacion.java
@@ -2,12 +2,14 @@ package com.ggo.juegodemate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -29,6 +31,31 @@ public class Multiplicacion extends AppCompatActivity {
 
     public void random(View v){
         numero.setText( String.valueOf(new Random().nextInt(10 - 1 + 1) + 1) );
+    }
+
+    public void empezar(View v){
+
+        if(check_number()){
+            Intent intent = new Intent( this, JuegoMultiplicacion.class);
+            intent.putExtra("numero", Integer.valueOf(String.valueOf(numero.getText())));
+            startActivity(intent);
+        }else{
+            error("Solo se puede un número del 1-10.");
+        }
+
+    }
+
+    public boolean check_number(){
+        int num = Integer.valueOf(String.valueOf(numero.getText()));
+        if (num > 0 && num < 11){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void error(String mensaje){
+        Toast.makeText(getApplicationContext(),mensaje, Toast.LENGTH_LONG).show();
     }
 
 
